4aa7afec2fdf19f9aabf4c530bcd17bf3d9cf3c0
Venesseus <satriayusufrafaly@gmail.com>
2023-06-08 02:31:11 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jetcoffee/ui/theme/Theme.kt
@@ -0,0 +1,35 @@
+package com.example.jetcoffee.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = LightCoffeeBrown,
+    primaryVariant = LightCoffeeBrown,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColors(
+    primary = CoffeeBrown,
+    primaryVariant = CoffeeBrown,
+    secondary = Teal200
+)
+
+@Composable
+fun JetCoffeeTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
